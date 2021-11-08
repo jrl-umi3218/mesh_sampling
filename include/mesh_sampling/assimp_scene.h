ca3a4c03d1c3cd607b2389d7319d2bae63888935
Arnaud Tanguy <arn.tanguy@gmail.com>
2021-11-08 19:01:35 +0100

    [export] Supports binary export format

--- include/mesh_sampling/assimp_scene.h
+++ include/mesh_sampling/assimp_scene.h
@@ -14,14 +14,8 @@ namespace bfs = boost::filesystem;
 namespace mesh_sampling
 {
 
-class ASSIMPScene
+struct ASSIMPScene
 {
-  // The importer will automatically delete the scene
-  Assimp::Importer importer;
-  const aiScene * scene_;
-  std::string modelPath_;
-
-public:
   ASSIMPScene(const std::string & model_path) : modelPath_(model_path)
   {
     loadScene();
@@ -43,29 +37,13 @@ public:
     return scene_;
   }
 
-  static std::vector<std::string> supportedExportFormats()
-  {
-    std::vector<std::string> supportedFormats;
-    Assimp::Exporter exporter;
-    auto numExporters = exporter.GetExportFormatCount();
-    for(auto i = 0u; i < numExporters; i++)
-    {
-      const aiExportFormatDesc * format = exporter.GetExportFormatDescription(i);
-      supportedFormats.push_back(format->fileExtension);
-    }
-    return supportedFormats;
-  }
-
   /**
    * @brief Export the scene to a file in a format supported by ASSIMP
-   * @see supportedExportFormats for a list of supported extensions
    *
    * @param path Export path
    */
-  void exportScene(const std::string & path)
+  void exportScene(const std::string & path, const bool binary = true)
   {
-    Assimp::Exporter exporter;
-    auto numExporters = exporter.GetExportFormatCount();
     bfs::path out_path(path);
     auto ext = boost::algorithm::to_lower_copy(out_path.extension().string());
     if(ext.empty())
@@ -73,30 +51,13 @@ public:
       throw std::runtime_error("Could't export scene " + modelPath_ + " to " + path + ": invalid extension");
     }
     ext.erase(0, 1); // remove leading "."
-    // Find exporter id for this extension
-    int index = -1;
-    for(auto i = 0u; i < numExporters; i++)
-    {
-      const aiExportFormatDesc * format = exporter.GetExportFormatDescription(i);
-      if(ext == format->fileExtension)
-      {
-        index = i;
-        break;
-      }
-    }
-    if(index == -1)
-    { // Unsupported export format
-      std::string s;
-      const auto supportedFormats = supportedExportFormats();
-      std::ostringstream ss;
-      std::copy(supportedFormats.begin(), supportedFormats.end() - 1, std::ostream_iterator<std::string>(ss, ", "));
-      throw std::runtime_error("Could't export: unsupported format " + ext + "\nSupported formats are: " + ss.str());
-    }
-    const aiExportFormatDesc * format = exporter.GetExportFormatDescription(index);
-    aiReturn ret = exporter.Export(scene_, format->id, path);
+    if(binary) ext += "b"; // ASSIMP suffixes binary export formats with "b"
+    Assimp::Exporter exporter;
+    aiReturn ret = exporter.Export(scene_, ext, path);
     if(ret != AI_SUCCESS)
     {
-      throw std::runtime_error("ASSIMP failed to export scene " + modelPath_ + " to " + path);
+      throw std::runtime_error("ASSIMP failed to export scene " + modelPath_ + " to " + path + ": "
+                               + exporter.GetErrorString());
     }
   }
 
@@ -113,6 +74,12 @@ protected:
       throw std::runtime_error(importer.GetErrorString());
     }
   }
+
+protected:
+  // The importer will automatically delete the scene
+  Assimp::Importer importer;
+  const aiScene * scene_;
+  std::string modelPath_;
 };
 
 } // namespace mesh_sampling
