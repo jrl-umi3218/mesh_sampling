2cca1f8977e87059a184d8e111245a1e102fb1ec
ThomasDuvinage <toto.duvinage@gmail.com>
2025-06-04 15:42:41 +0900

    feat(utils): consider folder as input and export convexes

--- utils/mesh_sampling.cpp
+++ utils/mesh_sampling.cpp
@@ -29,7 +29,8 @@ int main(int argc, char ** argv)
   desc.add_options()
     ("help", "Produce this message")
     ("in", po::value<std::string>(), "Input mesh (supported by ASSIMP)")
-    ("out", po::value<std::string>(), "Output file (ply, pcd, qc, stl)")
+    ("out", po::value<std::string>()->default_value({}), "Output file (ply, pcd, qc, stl)")
+    ("convex", po::value<std::string>()->default_value({}), "OUtput convex directory")
     ("samples", po::value<unsigned>()->default_value(10000), "Number of points to sample")
     ("scale", po::value<float>()->default_value(1.0), "Scale factor applied to the mesh")
     ("type", po::value<std::string>()->default_value("xyz_rgb_normal"), "Type of cloud to generate (xyz, xyz_rgb, xyz_rgb_normal)")
@@ -39,7 +40,7 @@ int main(int argc, char ** argv)
 
   po::positional_options_description pos;
   pos.add("in", 1);
-  pos.add("out", 1);
+  // pos.add("out", 1);
 
   // parse arguments and save them in the variable map (vm)
   po::store(po::command_line_parser(argc, argv).options(desc).positional(pos).run(), vm);
@@ -56,16 +57,21 @@ int main(int argc, char ** argv)
 
   std::string in_path = vm["in"].as<std::string>();
   std::string out_path = vm["out"].as<std::string>();
+  fs::path convex_path = vm["convex"].as<std::string>();
   fs::path in_p(in_path);
   fs::path out_p(out_path);
   const auto out_extension = boost::algorithm::to_lower_copy(out_p.extension().string());
   const unsigned N = vm["samples"].as<unsigned>();
   const std::string cloud_type = vm["type"].as<std::string>();
   const bool binary_format = vm["binary"].as<bool>();
-
+  
   MeshSampling mesh_sampler(in_p);
 
-  mesh_sampler.check_supported(supported_cloud_type, cloud_type);
+  if(!mesh_sampler.check_supported(cloud_type)){\
+    std::cerr << "Type not suppported : ";
+    std::copy(mesh_sampling::supported_cloud_type.begin(), mesh_sampling::supported_cloud_type.end(), std::ostream_iterator<std::string>(std::cerr, ", "));
+    std::cerr << std::endl;
+  }
 
   if(convert)
   {
@@ -75,20 +81,27 @@ int main(int argc, char ** argv)
   {
     if(cloud_type == "xyz")
     {
-      auto mesh = mesh_sampler.create_cloud<pcl::PointXYZ>(mesh_sampler.mesh(in_p)->scene(), N, out_p, binary_format);
-      mesh_sampler.create_convex(mesh, "/tmp/test-ch.txt");
+      auto mesh = mesh_sampler.create_clouds<pcl::PointXYZ>(N, out_p, ".qc", binary_format);
+      if(!convex_path.empty())
+        mesh_sampler.create_convexes<pcl::PointXYZ>(mesh, convex_path);
     }
     else if(cloud_type == "xyz_rgb")
     {
-      mesh_sampler.create_cloud<pcl::PointXYZRGB>(mesh_sampler.mesh(in_p)->scene(), N, out_p, binary_format);
+      auto mesh = mesh_sampler.create_clouds<pcl::PointXYZRGB>(N, out_p, ".qc", binary_format);
+      if(!convex_path.empty())
+        mesh_sampler.create_convexes<pcl::PointXYZRGB>(mesh, convex_path);
     }
     else if(cloud_type == "xyz_normal")
     {
-      mesh_sampler.create_cloud<pcl::PointNormal>(mesh_sampler.mesh(in_p)->scene(), N, out_p, binary_format);
+      auto mesh = mesh_sampler.create_clouds<pcl::PointNormal>(N, out_p, ".qc", binary_format);
+      if(!convex_path.empty())
+        mesh_sampler.create_convexes<pcl::PointNormal>(mesh, convex_path);
     }
     else if(cloud_type == "xyz_rgb_normal")
     {
-      mesh_sampler.create_cloud<pcl::PointXYZRGBNormal>(mesh_sampler.mesh(in_p)->scene(), N, out_p, binary_format);
+      auto mesh = mesh_sampler.create_clouds<pcl::PointXYZRGBNormal>(N, out_p, ".qc", binary_format);
+      if(!convex_path.empty())
+        mesh_sampler.create_convexes<pcl::PointXYZRGBNormal>(mesh, convex_path);
     }
   }
 
